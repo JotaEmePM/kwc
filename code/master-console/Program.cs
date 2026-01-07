6c44c525bf0d379ee7aa51502dec2b800b1bf806
Jose Miguel Perez <jotaemepm@outlook.com>
2026-01-07 15:19:41 -0300

    Master Console Impl y basic animation schema.

--- code/master-console/Program.cs
+++ code/master-console/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.WriteLine("APP Test de comunicacion RS485 to KWC Slaves\n");
+while (true)
+{
+    //Console.Clear();
+
+    // Display available COM ports
+    string[] ports = SerialPort.GetPortNames();
+    if (ports.Length == 0)
+    {
+        Console.WriteLine("No hay puertos COM disponibles en el sistema.");
+        break;
+    }
+
+    Console.WriteLine("Puertos COM disponibles:");
+    foreach (var p in ports)
+    {
+        Console.WriteLine($"  - {p}");
+    }
+    Console.WriteLine();
+
+    Console.Write("Ingresa el puerto COM asignado al rs485: ");
+
+    string? port = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(port))
+    {
+        Console.WriteLine("Debe ingresar una opcion...");
+        break;
+    }
+
+    while (true)
+    {
+        try
+        {
+            Console.Write("Ingrese comando: ");
+            string cmd = Console.ReadLine();
+
+            using var sp = new SerialPort(port, 9600, Parity.None, 8, StopBits.One);
+            sp.Open();
+            sp.WriteLine(cmd);
+            sp.Close();
+            Console.WriteLine($"Mensaje enviado exitosamente a {port}\n");
+        }
+        catch (System.IO.FileNotFoundException ex)
+        {
+            Console.WriteLine($"Error: El puerto '{port}' no existe o no está disponible.");
+            Console.WriteLine($"Detalles: {ex.Message}\n");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al comunicarse con el puerto: {ex.Message}\n");
+        }
+    }
+}
+
+
--- code/master/main.py
+++ code/master/main.py
@@ -9,6 +9,7 @@ name = 'Kinetic Wall Clock'
 in_movement = False
 
 message = ''
+firs_animation = True
 
 comm = None  # Acceso a protocolo RS485
 conf = None  # Acceso a archivos de configuracion
@@ -16,9 +17,13 @@ conf = None  # Acceso a archivos de configuracion
 
 def init_system():
     global comm, conf
+
+    # Init config data
     config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
     conf = Config(config_path)
+
     try:
+        # Check if communication between RS485 is enabled.
         if conf.config().getboolean('app', 'send_to_rs485'):
             print('Initializing RS485 Communication...')
             comm = Communication()
@@ -31,11 +36,20 @@ def init_system():
 def update():
     config = conf.config()
     # print(config['app']['send_to_rs485'])
-    if comm:
+    if can_send():
+
         comm.send_message('PING')
         response = comm.receive_data()
         print(f'Response: {response}')
         comm.close()
+
+
+def can_send():
+    result = True
+    if not comm:
+        result = False
+
+    return result
 
 
 if __name__ == '__main__':
